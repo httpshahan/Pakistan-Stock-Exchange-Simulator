6eb7fcda9a2ba5a7c3f4036cf155070411fc3eac
httpshahan <shahankhushdil@gmail.com>
2024-01-16 19:58:13 +0500

    visual changes in to searchBar and Top NavBar

--- client/src/components/NavBar/TopNabar.jsx
+++ client/src/components/NavBar/TopNabar.jsx
@@ -6,13 +6,15 @@ function TopNavbar() {
   const userName = localStorage.getItem("userName");
     
   return (
-    <div class="flex items-center justify-between h-16 bg-gray-800 p-8">
-      <div class="top-navbar-left">
+    <div className="flex items-center justify-between h-16 bg-gray-800 p-8">
+      <div className="top-navbar-left">
       </div>
-      <div class="top-navbar-right flex items-center">
-        <SearchBar />
-        <h1 class="text-white ml-4"> {userName} </h1>
-        <button class="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      <div className = "w-full m-10">
+      <SearchBar />
+      </div>
+      <div className="top-navbar-right flex items-center justify-between">
+        <h1 className="text-white ml-10"> {userName} </h1>
+        <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={() => {
             localStorage.removeItem("token");
             localStorage.removeItem("userId");
