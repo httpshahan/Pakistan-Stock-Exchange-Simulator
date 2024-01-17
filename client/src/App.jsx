b4abedcd55a3178e70a06d1dd18d9933d98fede4
httpshahan <shahankhushdil@gmail.com>
2024-01-17 08:28:47 +0500

    " "

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AdminLogin from './components/Auth/AdminLogin';
-import Register from './components/Auth/Register';
-import Login from './components/Auth/Login';
-import MarketWatch from './components/MarketWatch/MarketWatch';
-import Dashboard from './components/DashBoard';
-import StockDetail from './components/Stock/StockDetail';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AdminLogin from "./components/Auth/AdminLogin";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+import MarketWatch from "./components/MarketWatch/MarketWatch";
+import Dashboard from "./components/DashBoard";
+import StockDetail from "./components/Stock/StockDetail";
 
 function App() {
-
-    return (
-      <Router>
-        <Routes>
+  return (
+    <Router>
+      <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/market-watch" element={<MarketWatch />} />
         <Route path="/stock/:symbol" element={<StockDetail />} />
-        </Routes>
-      </Router>
-    );
-  
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
