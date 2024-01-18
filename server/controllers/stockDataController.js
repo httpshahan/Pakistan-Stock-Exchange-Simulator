c531c85b2971bad8196a84a80048b275e0b4d55b
httpshahan <shahankhushdil@gmail.com>
2024-01-18 21:21:22 +0500

    make routes for topAdvancers, decliners and active stocks

--- server/controllers/stockDataController.js
+++ server/controllers/stockDataController.js
@@ -1,5 +1,11 @@
 const e = require("express");
-const { getAllStocks, getStockBySymbol } = require("../models/stockData");
+const {
+  getAllStocks,
+  getStockBySymbol,
+  getTopStock,
+  getDeclinerStock,
+  getActiveStocks,
+} = require("../models/stockData");
 
 const getStocks = async (req, res) => {
   try {
@@ -15,17 +21,69 @@ const getStocks = async (req, res) => {
 };
 
 const getStock = async (req, res) => {
-    try {
-        const result = await getStockBySymbol(req.params.symbol);
-        if (!result) {
-            return res.status(404).json({ error: 'Stock not found' });
-        }
-        else{
-            res.status(200).json({message:'Succesfully Get the Stock', data: result});
-        }
-    } catch (error) {
-        return res.status(500).json({ error: 'Internal server error' });
+  try {
+    const result = await getStockBySymbol(req.params.symbol);
+    if (!result) {
+      return res.status(404).json({ error: "Stock not found" });
+    } else {
+      res
+        .status(200)
+        .json({ message: "Succesfully Get the Stock", data: result });
     }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
 
-module.exports = { getStocks, getStock };
+const getTopStocks = async (req, res) => {
+  try {
+    const result = await getTopStock();
+    if (!result) {
+      return res.status(404).json({ error: "Stock not found" });
+    } else {
+      res
+        .status(200)
+        .json({ message: "Succesfully Get the Top Stock", data: result });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const getDeclinerStocks = async (req, res) => {
+  try {
+    const result = await getDeclinerStock();
+    if (!result) {
+      return res.status(404).json({ error: "Stock not found" });
+    } else {
+      res
+        .status(200)
+        .json({ message: "Succesfully Get the Decliner Stock", data: result });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const activeStocks = async (req, res) => {
+  try {
+    const result = await getActiveStocks();
+    if (!result) {
+      return res.status(404).json({ error: "Stock not found" });
+    } else {
+      res
+        .status(200)
+        .json({ message: "Succesfully Get the Active Stock", data: result });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = {
+  getStocks,
+  getStock,
+  getTopStocks,
+  getDeclinerStocks,
+  activeStocks,
+};
--- server/routes/stockRoutes.js
+++ server/routes/stockRoutes.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const stockController = require('../controllers/stockDataController');
+const stockController = require("../controllers/stockDataController");
 
 // Define the route to get all stocks
-router.get('/getStocks', stockController.getStocks);
+router.get("/getStocks", stockController.getStocks);
 
 // Define the route to get a stock by symbol
-router.get('/getStock/:symbol', stockController.getStock);
+router.get("/getStock/:symbol", stockController.getStock);
+router.get("/topStocks/", stockController.getTopStocks);
+router.get("/declinerStocks/", stockController.getDeclinerStocks);
+router.get("/activeStocks/", stockController.activeStocks);
 
 module.exports = router;
