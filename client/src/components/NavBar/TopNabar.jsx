310bbededec6c34d6e6ef575c3203427e51bbebc
httpshahan <shahankhushdil@gmail.com>
2024-01-22 18:52:35 +0500

    cosmetic changes

--- client/src/components/NavBar/TopNabar.jsx
+++ client/src/components/NavBar/TopNabar.jsx
@@ -8,38 +8,42 @@ function TopNavbar() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <div className="flex items-center justify-between h-16 bg-gray-800 p-8">
-      <div className="top-navbar-left"></div>
-      <div className="w-full m-10">
-        <SearchBar />
+    <div className="flex items-center h-16 p-8">
+      <div className="w-full">
+        <p className="text-[#2C2C2C] text-xl font-medium">Hello, {userName}</p>
       </div>
-      <div className="top-navbar-right flex items-center justify-between">
-        {/* Dropdown Trigger */}
-        <div className="relative ml-10">
-          <button
-            onClick={() => setDropdownOpen(!isDropdownOpen)}
-            className="flex items-center text-white"
-          >
-            {userName}
-            <span className="ml-2">&#9662;</span> {/* Dropdown symbol */}
-          </button>
+      <div className="flex items-center justify-between gap-5">
+        <div className="w-full">
+          <SearchBar />
+        </div>
+        <div className="flex items-center">
+          {/* Dropdown Trigger */}
+          <div className="relative w-auto">
+            <button
+              onClick={() => setDropdownOpen(!isDropdownOpen)}
+              className="flex items-center w-max"
+            >
+              {userName}
+              <span className="">&#9662;</span> {/* Dropdown symbol */}
+            </button>
 
-          {/* Dropdown Content */}
-          {isDropdownOpen && (
-            <div className="absolute right-0 mt-2 bg-white rounded-md shadow-md min-w-max">
-              <button
-                className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left"
-                onClick={() => {
-                  localStorage.removeItem("token");
-                  localStorage.removeItem("userId");
-                  localStorage.removeItem("userName");
-                  window.location.href = "/";
-                }}
-              >
-                Logout
-              </button>
-            </div>
-          )}
+            {/* Dropdown Content */}
+            {isDropdownOpen && (
+              <div className="absolute right-0 mt-2 bg-white rounded-md shadow-md min-w-max">
+                <button
+                  className="block px-4 py-2 mt-4 text-gray-800 hover:bg-gray-200 w-full text-left"
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    localStorage.removeItem("userId");
+                    localStorage.removeItem("userName");
+                    window.location.href = "/";
+                  }}
+                >
+                  Logout
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
