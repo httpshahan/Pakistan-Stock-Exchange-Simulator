ffdd4cd93e3ad4e52399f9f1117296461e29e0f7
httpshahan <shahankhushdil@gmail.com>
2024-01-23 18:23:31 +0500

    Trade (Buy/sell) routes and controller

---
+++ server/routes/tradeRoute.js
@@ -0,0 +1,12 @@
+// // routes/transactionsRoutes.js
+// const express = require('express');
+// const router = express.Router();
+// const transactionsController = require('../controllers/tradeController');
+
+// // Buy stock
+// router.post('/buy/:userId', transactionsController.buyStock);
+
+// // Sell stock
+// router.post('/sell/:userId', transactionsController.sellStock);
+
+// module.exports = router;
