e269c6eca607a284c3721957ce5781c6ef9bd533
httpshahan <shahankhushdil@gmail.com>
2024-02-10 13:45:04 +0500

    Refactor login page layout and add register button

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -16,12 +16,11 @@ const Login = () => {
       setInvalid(false);
       setInvalidPassword(false);
 
-      console.log(data);
       sessionStorage.setItem("token", data.token);
       sessionStorage.setItem("userId", data.userId);
       sessionStorage.setItem("username", data.username);
       sessionStorage.setItem("balance", data.balance);
-      console.log("Login Success");
+      
       alert("Login Success");
       navigate("/dashboard");
     } catch (error) {
@@ -38,77 +37,88 @@ const Login = () => {
     }
   };
 
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-gray-100 p-8 rounded shadow-md w-96">
-        <h2 className="text-2xl font-semibold mb-4">Login</h2>
-        <div
-          className="text-red-500 mb-4"
-          style={{ display: invalid ? "block" : "none" }}
-        >
-          <p className="text-sm font-sans"> Invalid Credentials</p>
-        </div>
-        <form>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="abc@example.com"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={`mt-1 p-2 w-full border rounded-md ${
-                invalid ? "border-red-500" : ""
-              }`}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className={`mt-1 p-2 w-full border rounded-md ${
-                invalidPassword ? "border-red-500" : ""
-              }`}
-            />
-            <div
-              className="text-red-500 mt-2"
-              style={{ display: invalidPassword ? "block" : "none" }}
-            >
-              <p className="text-sm font-sans"> Invalid Password</p>
-            </div>
-          </div>
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-400 to-green-600 text-gray-800">
+      <div className="w-1/2 flex justify-center items-center">
+        <div className="flex flex-col items-center">
+          <h2 className="text-4xl font-bold mb-4">Pakistan Stock Exchange</h2>
+          <p className="text-lg mb-4">"Invest in your future."</p>
           <button
-            type="button"
-            onClick={handleLogin}
-            className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+            onClick={handleRegisterClick}
+            className="text-lg underline focus:outline-none hover:text-yellow-100 transition-colors duration-300"
           >
-            Login
+            Register now
           </button>
-        </form>
-        <div className="mt-4">
-          <p className="text-sm text-gray-600">
-            Not registered yet?{" "}
-            <a href="/register" className="text-blue-500">
-              Register
-            </a>
-          </p>
         </div>
       </div>
+      <div className="flex justify-center items-center w-1/2">
+        <div className="bg-white p-8 rounded shadow-md w-96">
+          <h2 className="text-3xl font-semibold mb-4 text-green-500">Login</h2>
+          <div
+            className="text-red-500 mb-4"
+            style={{ display: invalid ? "block" : "none" }}
+          >
+            <p className="text-sm font-sans">Invalid Credentials</p>
+          </div>
+          <form>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 text-sm font-medium"
+              >
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="abc@example.com"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={`mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:border-green-400 ${
+                  invalid ? "border-red-500" : ""
+                }`}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 text-sm font-medium"
+              >
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className={`mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:border-green-400 ${
+                  invalidPassword ? "border-red-500" : ""
+                }`}
+              />
+              <div
+                className="text-red-500 mt-2"
+                style={{ display: invalidPassword ? "block" : "none" }}
+              >
+                <p className="text-sm font-sans">Invalid Password</p>
+              </div>
+            </div>
+            <button
+              type="button"
+              onClick={handleLogin}
+              className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+      
     </div>
   );
 };
--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -7,6 +8,7 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordMatch, setPasswordMatch] = useState(true);
   const [confirmation, setConfirmation] = useState(false);
+  const [error, setError] = useState(null);
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
@@ -20,6 +22,12 @@ const Register = () => {
   };
 
   const handleRegister = async () => {
+    // Form validation
+    if (!name || !email || !password || !confirmPassword) {
+      setError("All fields are required");
+      return;
+    }
+
     try {
       console.log("Registering user...");
       const userData = {
@@ -37,18 +45,11 @@ const Register = () => {
       const data = await response.json();
       if (response.status === 400) {
         console.log("User already exists");
-        setConfirmPassword("");
-        setConfirmation(false);
-        setPassword("");
-        setEmail("");
-        setName("");
-        alert("User already exists");
+        setError("User already exists");
         return;
-      }
-      else if (response.status === 200) {
+      } else if (response.status === 200) {
         console.log("User registered successfully");
         window.location.href = "/";
-        alert("User registered successfully");
         return;
       }
       console.log(data);
@@ -58,109 +59,109 @@ const Register = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-gray-100 p-8 rounded shadow-md w-96">
-        <h2 className="text-2xl font-semibold mb-4">Register</h2>
-        <form>
-          <div className="mb-4">
-            <label
-              htmlFor="name"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Name:
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="mt-1 p-2 w-full border rounded-md"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 p-2 w-full border rounded-md"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              onChange={handlePasswordChange}
-              className="mt-1 p-2 w-full border rounded-md"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="confirmPassword"
-              className="block text-gray-700 text-sm font-medium"
-            >
-              Confirm Password:
-            </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              value={confirmPassword}
-              onChange={handleConfirmPasswordChange}
-              className={`mt-1 p-2 w-full border rounded-md ${
-                confirmation
-                  ? passwordMatch
-                    ? "border-green-500"
-                    : "border-red-500"
-                  : ""
-              }`}
-            />
-            {confirmation && (
-              <span
-                className={`${
-                  passwordMatch ? "text-green-500" : "text-red-500"
-                } ml-2`}
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-400 to-green-600 ">
+      <div className="flex justify-center items-center w-1/2">
+        <div className="bg-white p-8 rounded shadow-md w-96">
+          <h2 className="text-3xl font-semibold mb-4 text-green-500">Register</h2>
+          {error && (
+            <div className="text-red-500 mb-4">
+              <p className="text-sm font-sans">{error}</p>
+            </div>
+          )}
+          <form>
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-gray-700 text-sm font-medium"
               >
-                {passwordMatch
-                  ? "✓ Passwords match"
-                  : "✗ Passwords do not match"}
-              </span>
-            )}
-          </div>
-          <button
-            type="button"
-            onClick={handleRegister}
-            disabled={!passwordMatch} // Disable button if passwords don't match
-            className={`bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 ${
-              !passwordMatch && "cursor-not-allowed opacity-50"
-            }`}
-          >
-            Register
-          </button>
-        </form>
-        <div className="mt-4">
-          <p className="text-sm text-gray-600">
-            Already registered?{" "}
-            <a href="/" className="text-blue-500">
-              Login
-            </a>
-          </p>
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:border-green-400"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 text-sm font-medium"
+              >
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:border-green-400"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 text-sm font-medium"
+              >
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={handlePasswordChange}
+                className={`mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:border-green-400 ${
+                  confirmation ? (passwordMatch ? "border-green-500" : "border-red-500") : ""
+                }`}
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="confirmPassword"
+                className="block text-gray-700 text-sm font-medium"
+              >
+                Confirm Password:
+              </label>
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+                className={`mt-1 p-2 w-full border rounded-md bg-gray-100 focus:outline-none focus:border-green-400 ${
+                  confirmation ? (passwordMatch ? "border-green-500" : "border-red-500") : ""
+                }`}
+                required
+              />
+              {confirmation && (
+                <span className={`${passwordMatch ? "text-green-500" : "text-red-500"} ml-2`}>
+                  {passwordMatch ? "✓ Passwords match" : "✗ Passwords do not match"}
+                </span>
+              )}
+            </div>
+            <button
+              type="button"
+              onClick={handleRegister}
+              disabled={!passwordMatch}
+              className={`w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600 ${!passwordMatch && "cursor-not-allowed opacity-50"}`}
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="w-1/2 flex justify-center items-center">
+        <div className="flex flex-col items-center">
+          <h2 className="text-4xl font-bold mb-4">Pakistan Stock Exchange</h2>
+          <p className="text-lg mb-4">"Invest in your future."</p>
+          <Link to="/" className="text-lg underline focus:outline-none hover:text-yellow-100 transition-colors duration-300">Login now</Link>
         </div>
       </div>
     </div>
