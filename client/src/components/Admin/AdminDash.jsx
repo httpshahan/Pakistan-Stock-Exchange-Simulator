5ea9f280511837f51c7457084cd1b98dafbee280
httpshahan <shahankhushdil@gmail.com>
2024-02-15 14:38:28 +0500

    Refactor AdminDash component and remove unused files

--- client/src/components/Admin/AdminDash.jsx
+++ client/src/components/Admin/AdminDash.jsx
@@ -1,41 +1,62 @@
 import React, { useState } from "react";
 
 import UserManagement from "./UserManag";
-import TransactionHistory from "./Transaction";
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@tremor/react";
+
 
 const AdminDash = () => {
+  const [showUsers, setShowUsers] = useState(true);
+  const [showTransactions, setShowTransactions] = useState(false);
+
+  const handleShowUsers = () => {
+    setShowUsers(true);
+    setShowTransactions(false);
+  };
+
+  const handleShowTransactions = () => {
+    setShowUsers(false);
+    setShowTransactions(true);
+  };
+
   return (
     <div className="flex h-screen bg-gray-100">
       <div className="flex-1 flex flex-col">
         <header className="flex justify-between">
           <h1 className="text-3xl font-bold p-6">Admin Dashboard</h1>
+          <nav className="flex">
+            <button className="px-4 py-2 rounded-lg m-6">Dashboard</button>
+            <button
+              className="px-4 py-2 rounded-lg m-6"
+              onClick={handleShowUsers}
+            >
+              Users
+            </button>
+            <button
+              className="px-4 py-2 rounded-lg m-6"
+              onClick={handleShowTransactions}
+            >
+              Transactions
+            </button>
+          </nav>
           <button className="bg-blue-500 text-white px-4 py-2 rounded-lg m-6">
             Logout
           </button>
         </header>
         <main className="flex-1 p-6 justify-center overflow-hidden">
-          <TabGroup>
-            <TabList>
-              <Tab>Users</Tab>
-              <Tab>Transactions</Tab>
-            </TabList>
-            <TabPanels className="overflow-x-auto">
-              <TabPanel>
-                <UserManagement />
-              </TabPanel>
-              <TabPanel>
-                <TransactionHistory />
-              </TabPanel>
-            </TabPanels>
-          </TabGroup>
+          <div className="overflow-x-auto mb-8">
+            {showUsers && <UserManagement />}
+            {showTransactions && (
+              <div className="overflow-x-auto p-8">
+                
+              </div>
+            )}
+          </div>
         </main>
 
-        <footer className="bg-white py-4 px-6 border-t">
+        {/* <footer className="bg-white py-4 px-6 border-t">
           <p className="text-sm text-gray-600">
             © 2024 Stock Exchange Simulator
           </p>
-        </footer>
+        </footer> */}
       </div>
     </div>
   );
