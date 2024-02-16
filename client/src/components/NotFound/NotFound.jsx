3705453e0684b3ba52ee51773f7c155c25118a9f
httpshahan <shahankhushdil@gmail.com>
2024-02-16 19:01:53 +0500

    Update NotFound component UI

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,28 +1,37 @@
 import React from "react";
-import SideNavbar from "../NavBar/SideNavBar";
-import TopNavbar from "../NavBar/TopNabar";
 
 const NotFound = () => {
   return (
-    <div className="flex h-screen bg-gray-100">
-      <SideNavbar />
-      <div className="flex flex-col flex-1">
-        <TopNavbar />
-        <div className="flex-1 flex items-center justify-center">
-          <div className="text-center">
-            <h1 className="text-4xl font-semibold text-stock-secondary mb-4">
-              404 Not Found
-            </h1>
-            <p className="text-hover-light mb-6">
-              Sorry, the page you are looking for does not exist.
-            </p>
-            <p className="text-stock-light">
-              <a href="/dashboard" className="text-stock-tertiary">
-                Return to the home page
-              </a>
-            </p>
-          </div>
-        </div>
+    <div class="bg-gray-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
+      <div class="bg-white border border-gray-200 flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl">
+        <p class="text-6xl md:text-7xl lg:text-9xl font-bold tracking-wider text-gray-300">
+          404
+        </p>
+        <p class="text-2xl md:text-3xl lg:text-5xl font-bold tracking-wider text-gray-500 mt-4">
+          Page Not Found
+        </p>
+        <p class="text-gray-500 mt-4 pb-4 border-b-2 text-center">
+          Sorry, the page you are looking for could not be found.
+        </p>
+        <a
+          href="/dashboard"
+          class="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 mt-6 rounded transition duration-150"
+          title="Return Dashboard"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span>Return Dashboard</span>
+        </a>
       </div>
     </div>
   );
