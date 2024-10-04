09fbbbfc05b9bdf296490bfbecbac2121042cd00
httpshahan <shahankhushdil@gmail.com>
2024-10-04 09:32:44 +0500

    refactor: Update email service configuration in otpService.js

--- server/services/otpService.js
+++ server/services/otpService.js
@@ -3,14 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "shahankhushdil@gmail.com",
-    pass: "abua xpzl opgg rdsv",
+    user: process.env.email,
+    pass: process.env.pass,
   },
 });
 
 function sendOTP(email, otp) {
   const mailOptions = {
-    from: "shahankhushdil+forgetpassword@gmail.com",
+    from: process.env.email,
     to: email,
     subject: "Password Reset OTP",
     text: `Your OTP for password reset is: ${otp}`,
